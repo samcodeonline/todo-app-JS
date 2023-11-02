f0f5ee340dfc746c18e819dcf07c703045b50480
sarim nadeem <115877996+samcodeonline@users.noreply.github.com>
2023-11-02 09:27:53 +0500

    UPDATA

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
 const title = document.getElementById("title");
 
-
 const description = document.getElementById("description");
 
 const form = document.querySelector("form");
 
 const container = document.querySelector(".container");
 
-const tasks = [];
+const tasks =  localStorage.getItem("tasks") ?  JSON.parse(localStorage.getItem("tasks")) : [];
 
+showAllTasks();
 
-function showAllTasks(){
-    tasks.forEach((value,index)=>{
-        const div = document.createElement("div");
-        div.setAttribute("class", "task");
-        const innerDiv = document.createElement("div");
-        div.append(innerDiv);
+function showAllTasks() {
+  tasks.forEach((value, index) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "task");
+    const innerDiv = document.createElement("div");
+    div.append(innerDiv);
 
-        const p = document.createElement("p");
+    const p = document.createElement("p");
 
-        p.innerText= value.title;
-        innerDiv.append(p);
-        const span = document.createElement("span");
-        span.innerText= value.description;
-        innerDiv.append(span);
+    p.innerText = value.title;
+    innerDiv.append(p);
+    const span = document.createElement("span");
+    span.innerText = value.description;
+    innerDiv.append(span);
 
+    const btn = document.createElement("button");
+    btn.setAttribute("class", "deleteBtn");
 
-        const btn = document.createElement("button");
-        btn.setAttribute("class", "deleteBtn");
+    btn.innerText = "-";
 
-        btn.innerText = "-";
+    btn.addEventListener("click", () => {
+      removeTasks();
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
 
+      showAllTasks();
+    });
 
-        div.append(btn);
+    div.append(btn);
 
-        container.append(div);
-
-    })
+    container.append(div);
+  });
 }
 
-
-function removeTasks(){
-    tasks.forEach(()=>{
-        const div = document.querySelector(".task");
-        div.remove();
-    })
+function removeTasks() {
+  tasks.forEach(() => {
+    const div = document.querySelector(".task");
+    div.remove();
+  });
 }
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    removeTasks();
+  removeTasks();
 
-    tasks.push({
-        title : title.value,
-        description : description.value,
-    })
-    console.log(tasks)
+  tasks.push({
+    title: title.value,
+    description: description.value,
+  });
 
-    showAllTasks();
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 
- 
-} )
+  showAllTasks();
+});
